5c21b79411add22b57c367c2ed9f28f37dd510c4
simonpok <66308088+simonpok@users.noreply.github.com>
2022-03-16 15:00:43 +0545

    Add files via upload

---
+++ unfinished.cpp
@@ -0,0 +1,29 @@
+//WAP to find largest no. of two class using friend function
+
+#include<iostream>
+using namespace std;
+
+class first{
+	public:
+		void setvalue(int a)
+		{
+			int1=a;
+		}
+};
+class second{
+	public:
+		void setvalue(int b)
+		{
+			int2=b;
+		}
+};
+
+int main()
+{
+	first obj;
+	second obj1;
+	
+}
+
+
+
