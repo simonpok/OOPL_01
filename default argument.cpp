f756c959223188cf9283d12b2388a80035b0cb93
simonpok <66308088+simonpok@users.noreply.github.com>
2022-03-16 14:53:55 +0545

    Add files via upload
    updated

--- default argument.cpp
+++ default argument.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
-
-int sum(int l, int b)
+class sum
+{
+	int l,b;
+	public:
+		int add(int l, int b)
 {
 	return (l*b);
 }
 
+		
+};
+
 int main()
 {
-	
-	cout << sum(10, 5) << endl;
+	sum d;
+	cout << d.add(10, 5) << endl;
 
 	
-	cout << sum(5, 5) << endl;
+	cout << d.add(5, 5) << endl;
 
 	
-	cout << sum(10, 2) << endl;
+	cout << d.add(10, 2) << endl;
 	return 0;
 }
-
