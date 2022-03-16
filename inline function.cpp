33098f1c5ef15e128906b72cff74f791fb74b078
simonpok <66308088+simonpok@users.noreply.github.com>
2022-03-16 14:54:25 +0545

    Add files via upload
    updated

--- inline function.cpp
+++ inline function.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
-inline int sum(int a, int b)
+
+class add 
+{
+	int a,b;
+	public:
+		inline int sum(int a, int b)
 {
 	return a+b;
 }
+		
+	
+};
+
 int main()
 {
-  int a,b;
+	add s;
+    int a,b;
     cout <<"Enter value for a: ";
     cin>>a;
-  cout<<"Enter value for b: ";
-  cin>>b;
-	cout << "The sum is: " << sum(a,b);
+    cout<<"Enter value for b: ";
+    cin>>b;
+	cout << "The sum is: " << s.sum(a,b);
 	return 0;
 }
